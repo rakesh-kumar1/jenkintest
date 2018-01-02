0f14530cf05ef6195245ac6e04a2277073c6dfce
rakesh-kumar1 <rakesh.143t@gmail.com>
2018-01-02 15:29:32 +0530

    added code

--- src/main/java/com/tcs/jenkins/TestClass.java
+++ src/main/java/com/tcs/jenkins/TestClass.java
@@ -5,8 +5,10 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("In the test class......");
 		System.out.println("Testing auto build in jenkins");
-		System.out.println(" Printing 1 to 10");
-		for(int i=0;i<=10;i++) {
+		System.out.println("Printing numbers from 1 to 15");
+		
+		for(int i=0;i<=15;i++) {
+			
 			System.out.println(+i);
 		}
 	}
