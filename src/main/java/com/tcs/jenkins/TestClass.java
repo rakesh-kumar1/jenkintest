6506c2747071d563bf01487300cb6d2fba1d220c
rakesh-kumar1 <rakesh.143t@gmail.com>
2018-01-02 13:25:36 +0530

    added new change

--- src/main/java/com/tcs/jenkins/TestClass.java
+++ src/main/java/com/tcs/jenkins/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 	
 	public static void main(String[] args) {
 		System.out.println("In the test class......");
+		System.out.println("Testing auto build in jenkins");
 	}
 
 }
