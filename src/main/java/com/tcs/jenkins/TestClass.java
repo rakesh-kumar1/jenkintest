84b51ae4227bf217fc87078383aeb4f32786d86d
rakesh-kumar1 <rakesh.143t@gmail.com>
2018-01-02 15:44:25 +0530

    changed in file

--- src/main/java/com/tcs/jenkins/TestClass.java
+++ src/main/java/com/tcs/jenkins/TestClass.java
@@ -5,9 +5,9 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("In the test class......");
 		System.out.println("Testing auto build in jenkins");
-		System.out.println("Printing numbers from 1 to 15");
+		System.out.println("Printing numbers from 1 to 10");
 		
-		for(int i=0;i<=15;i++) {
+		for(int i=0;i<=10;i++) {
 			
 			System.out.println(+i);
 		}
